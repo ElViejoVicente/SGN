2c978029d654eb573f442d08d85c221db9736e0b
Vicente Hernandez <vhernandez@consultoria-it.com>
2023-12-22 16:44:34 -0600

    clase lista expediente

--- SGN.Negocio/Expediente/DatosExpedientes.cs
+++ SGN.Negocio/Expediente/DatosExpedientes.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Dapper;
+using SGN.Negocio.ORM;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SGN.Negocio.Expediente
 {
-    class DatosExpedientes
+    public class DatosExpedientes
     {
+        protected String cnn = ConfigurationManager.AppSettings["sqlConn.ConnectionString"];
+
+        public List<ListaExpedientes> DameListaExpediente(DateTime fechaInicial, DateTime fechaFinal)
+        {
+            try
+            {
+                List<ListaExpedientes> resultado = new List<ListaExpedientes>();
+
+                using (var db = new SqlConnection(cnn))
+                {
+                    resultado = db.Query<ListaExpedientes>
+                        (
+                        sql: "sp_DameExpedientePorFecha", param: new
+                        {
+                            fechaInicial,
+                            fechaFinal
+
+                        }, commandType: CommandType.StoredProcedure
+                        ).ToList();
+                }
+                return resultado;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error al ejecutar sp_DameExpedientePorFecha , detalle: \n" + ex.Message, ex);
+            }
+        }
+
+
     }
 }
