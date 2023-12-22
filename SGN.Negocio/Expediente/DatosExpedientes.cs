beb9b652ef3de1efb4776a4ecce648bdf521d5e6
Vicente Hernandez <vhernandez@consultoria-it.com>
2023-12-22 14:01:49 -0600

    mas cositas de ajustes

--- SGN.Negocio/Expediente/DatosExpedientes.cs
+++ SGN.Negocio/Expediente/DatosExpedientes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SGN.Negocio.Expedientes
+namespace SGN.Negocio.Expediente
 {
     class DatosExpedientes
     {
--- SGN.Web/Expedientes/PanelExpedientes.aspx.cs
+++ SGN.Web/Expedientes/PanelExpedientes.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Web.Controles.Servidor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +8,29 @@ using System.Web.UI.WebControls;
 
 namespace SGN.Web.Expedientes
 {
-    public partial class PanelExpedientes : System.Web.UI.Page
+    public partial class PanelExpedientes : PageBase
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvExpedientes_DataBinding(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvExpedientes_CustomCallback(object sender, DevExpress.Web.ASPxGridViewCustomCallbackEventArgs e)
+        {
+
+        }
+
+        protected void gvExpedientes_ToolbarItemClick(object source, DevExpress.Web.Data.ASPxGridViewToolbarItemClickEventArgs e)
+        {
+
+        }
+
+        protected void gvExpedientes_HtmlDataCellPrepared(object sender, DevExpress.Web.ASPxGridViewTableDataCellEventArgs e)
         {
 
         }
