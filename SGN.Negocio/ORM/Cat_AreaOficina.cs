88840a20ee6a0fadb0dcbb33801e015e08c9bd32
ManuelDesion <edesion@consultoria-it.com>
2025-04-14 15:50:47 -0600

    catalogos
    de crearon los crud y clases de los catalogos de tipo inventario y areas

---
+++ SGN.Negocio/ORM/Cat_AreaOficina.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGN.Negocio.ORM
+{
+    public class Cat_AreaOficina
+    {
+        public int IdArea { get; set; }
+        public string TextoAreaOficina { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ SGN.Negocio/ORM/Cat_TipoInventario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGN.Negocio.ORM
+{
+    public class Cat_TipoInventario
+    {
+        public int idTipoInventario { get; set; }
+        public string TextoInventario { get; set; }
+        public bool Activo { get; set; }
+    }
+}
