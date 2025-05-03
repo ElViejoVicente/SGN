18a4e1ebb24d072b6060b786bd430dd15bc00562
ElViejoVicente <vhernandez@consultoria-it.com>
2025-05-02 19:28:52 -0600

    update dapper

--- SGN.Negocio/ORM/Expedientes.cs
+++ SGN.Negocio/ORM/Expedientes.cs
@@ -7,92 +7,54 @@ using System.Threading.Tasks;
 
 namespace SGN.Negocio.ORM
 {
-	public class Expedientes
-	{
-        // Datos ya no necesario ya que ahora estan en la tabla hoja de datos
-
-
-        //      public string numReciboPago { get; set; } = "";
-        //      public string numReciboPago2 { get; set; }
+    public class Expedientes
+    {
         public string IdExpediente { get; set; } = "";
         public int IdHojaDatos { get; set; } = 0;
         public string IdEstatus { get; set; } = "";
-        //      public int IdActo { get; set; } = 0;
-        //      public DateTime FechaIngreso { get; set; } = Constantes.FechaGlobal;
-        //      public string Faltantes { get; set; } = "";
-
-        //Expedientes
-        public string Otorga { get; set; } = "";
-		public string AfavorDe { get; set; } = "";
-		//public string OperacionProyectada { get; set; } = "";
-		public string UbicacionPredio { get; set; } = "";
-		
-
-        //Aviso preventivo
+        public string Otorga { get; set; }= "";
+        public string AfavorDe { get; set; } = "";
+        public string UbicacionPredio { get; set; } = "";
         public DateTime FechaElaboracion { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaEnvioRPP { get; set; } = Constantes.FechaGlobal;
-		public bool EsTramitePorSistema { get; set; } = false;
-		public DateTime FechaPagoBoleta { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaRecibidoRPP { get; set; } = Constantes.FechaGlobal;
-
-        //Proyecto
+        public DateTime FechaEnvioRPP { get; set; } = Constantes.FechaGlobal;
+        public bool EsTramitePorSistema { get; set; } = false;
+        public DateTime FechaPagoBoleta { get; set; } = Constantes.FechaGlobal; 
+        public DateTime FechaRecibidoRPP { get; set; } = Constantes.FechaGlobal;
         public string NombreProyectista { get; set; } = "";
-		public DateTime FechaAsignacionProyectista { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaPrevistaTerminoProyectista { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaAvisoPreventivo { get; set; } = Constantes.FechaGlobal;
-        public decimal ValorOperacion { get; set; } = 0;
-
-        public string ActividadVulnerable { get; set; } = "";
-
-        //Firmas
+        public DateTime FechaAsignacionProyectista { get; set; } = Constantes.FechaGlobal;  
+        public DateTime FechaPrevistaTerminoProyectista { get; set; } = Constantes.FechaGlobal;
+        public DateTime FechaAvisoPreventivo { get; set; } = Constantes.FechaGlobal;
+        public decimal ISR { get; set; } = 0;
         public string NotasFirma { get; set; } = "";
-		public int Escritura { get; set; } = 0;
-		public int Volumen { get; set; } = 0;
-        public bool AplicaTraslado { get; set; } = false;
-		public DateTime FechaRecepcionTerminoEscritura { get; set; } = Constantes.FechaGlobal;
-        public DateTime FirmaDeTraslado { get; set; } = Constantes.FechaGlobal;
-        public DateTime FechaDeOtorgamiento { get; set; } = Constantes.FechaGlobal;
-
-        //Aviso definitivo
+        public int Escritura { get; set; } = 0;
+        public int Volumen { get; set; } = 0;
+        public bool AplicaTraslado { get; set; }
+        public DateTime FechaRecepcionTerminoEscritura { get; set; } = Constantes.FechaGlobal;  
         public DateTime FechaElaboracionDefinitivo { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaEnvioRPPDefinitivo { get; set; } = Constantes.FechaGlobal;
-		public bool EsTramitePorSistemaDefinitivo { get; set; } = false;
-		public DateTime FechaPagoBoletaDefinitivo { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaRecibidoRPPDefinitivo { get; set; } = Constantes.FechaGlobal;
-
-        //Escrituracion
+        public DateTime FechaEnvioRPPDefinitivo { get; set; } = Constantes.FechaGlobal;
+        public bool EsTramitePorSistemaDefinitivo { get; set; } = false;
+        public DateTime FechaPagoBoletaDefinitivo { get; set; } = Constantes.FechaGlobal;
+        public DateTime FechaRecibidoRPPDefinitivo { get; set; } = Constantes.FechaGlobal;  
         public DateTime FechaRecibioTraslado { get; set; } = Constantes.FechaGlobal;
         public DateTime FechaAsignacionMesa { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaTerminoMesa { get; set; } = Constantes.FechaGlobal;
-
-        //Entrega
+        public DateTime FechaTerminoMesa { get; set; } = Constantes.FechaGlobal;
         public string ObservacionesEngrega { get; set; } = "";
-        public bool RegistroEntrega { get; set; } = false;       
+        public bool RegistroEntrega { get; set; } = false;
         public DateTime FechaRegistroEntrega { get; set; } = Constantes.FechaGlobal;
-		public DateTime FechaBoletaPagoRegistroEntrega { get; set; } = Constantes.FechaGlobal;
-        public DateTime FechaRegresoRegistro { get; set; } = Constantes.FechaGlobal;      
-		public DateTime FechaSalida { get; set; } = Constantes.FechaGlobal;
-		public string ObservacionesTramiteTerminado { get; set; } = "";
-        public DateTime FechaAutorizacion { get; set; } = Constantes.FechaGlobal;
-
-        //Contabilidad
-
-        public decimal ISR { get; set; } = 0;
-
+        public DateTime FechaBoletaPagoRegistroEntrega { get; set; } = Constantes.FechaGlobal;
+        public DateTime FechaRegresoRegistro { get; set; } = Constantes.FechaGlobal;     
+        public DateTime FechaSalida { get; set; } = Constantes.FechaGlobal;
+        public string ObservacionesTramiteTerminado { get; set; } = "";
+        public decimal ValorOperacion { get; set; } = 0;
         public decimal ISRcalculado { get; set; } = 0;
-
         public decimal AvaluoCatastral { get; set; } = 0;
-
         public decimal AvaluoFiscal { get; set; } = 0;
-
         public decimal AvaluoComercial { get; set; } = 0;
-
+        public string ActividadVulnerable { get; set; } = "";   
+        public DateTime FirmaDeTraslado { get; set; } = Constantes.FechaGlobal;
+        public DateTime FechaDeOtorgamiento { get; set; } = Constantes.FechaGlobal;
         public DateTime FechaDeAvaluo { get; set; } = Constantes.FechaGlobal;
-
+        public DateTime FechaAutorizacion { get; set; } = Constantes.FechaGlobal;
         public DateTime FechaPagoAvaluo { get; set; } = Constantes.FechaGlobal;
-
-        
-
-
     }
 }
